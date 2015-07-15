df6e7ba2e64db468cec999401d38c2b918838db8
John Thomas Connor <jt.connor88@gmail.com>
2015-07-15 02:06:11 -0700

    minimal

---
+++ js/expando.js
@@ -0,0 +1,18 @@
+// Google Expando Method
+// =====================================================
+
+function googleExpandoToggle() {
+  $(this).toggleClass('active');
+  $(this).next().toggleClass('active');
+
+  // ARIA
+  $expando_card.attr('aria-hidden') === 'true' ? $expando.attr('aria-hidden', 'false') : $expando.attr('aria-hidden', 'true');
+}
+
+
+// Google Expando Event
+// =====================================================
+
+$('.google-expando__icon').on('click', function() {
+  googleExpandoToggle.call(this);
+});
