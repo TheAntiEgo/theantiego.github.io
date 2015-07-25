257ba17868651b784fb7ff25a92f37d76493a1ae
John Thomas Connor <jt.connor88@gmail.com>
2015-07-24 20:48:33 -0700

    flat

--- scrolltop.js
+++ scrolltop.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     $('.scrollup').click(function () {
         $("html, body").animate({
             scrollTop: 0
-        }, 900);
+        }, 1200);
         return false;
     });
 
